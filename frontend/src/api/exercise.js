705f7f5899fc79de18199b732ac6b866cf97f28a
정민 <jmeve24@naver.com>
2024-08-15 21:32:41 +0900

    🐛 디버깅

--- frontend/src/api/exercise.js
+++ frontend/src/api/exercise.js
@@ -113,11 +113,14 @@ export const registerFeedbackToAI = async (record, exerciseName) => {
       "Content-Type": "multipart/form-data",
     },
   });
+  console.log("ai에서 받은 영상", res);
+  console.log("데이터 형식확인", res.data);
   return res.data;
 };
 
 export const registerFeedback = async (memo, exerciseId, record, createdAt) => {
   const formData = new FormData();
+  console.log("받은 영상", record);
   formData.append(`memo`, memo);
   formData.append(`exerciseName`, exerciseId);
   formData.append(`record`, record);
